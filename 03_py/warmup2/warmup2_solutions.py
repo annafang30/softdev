ff0859df63ebf1ac497759d530fdba6d8fb376b1
Anna Fang <78171295+annafang30@users.noreply.github.com>
2022-09-19 20:09:55 -0400

    added currently working warmup2 solutions

--- 03_py/warmup2/warmup2_solutions.py
+++ 03_py/warmup2/warmup2_solutions.py
@@ -1,1 +1,12 @@
+# string times 
+def string_times(str, n):
+  return n * str
+
+# front times
+def front_times(str, n):
+  if len(str) > 2:
+    return n * str[0:3]
+  else:
+    return n * str
+
 
