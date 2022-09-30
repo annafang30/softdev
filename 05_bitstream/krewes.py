4227bfe01ad9c610e49fbf04d05397a3576aa58c
Anna Fang <78171295+annafang30@users.noreply.github.com>
2022-09-30 11:27:39 -0400

    update with new code from partner

--- 05_bitstream/krewes.py
+++ 05_bitstream/krewes.py
@@ -1,3 +1,32 @@
-f = open('krewes.txt')
-contents = f.read()
-print(contents)
+'''
+DISCO:
+
+QCC:
+
+OPS SUMMARY:
+'''
+
+file = open('krewes.txt')
+contents = file.read()
+    
+
+
+krewes = {}
+
+def create_dict(str):
+    strs = str.split('@@@')
+    print(strs)
+    strs = strs[:-1]
+    
+    for x in strs:
+        values = x.split('$$$')
+        key = int(values[0])
+        name = values[1]
+        ducky = values[2]
+        
+        krewes[key].append(name)
+        krewes[key].append(ducky)
+        
+
+create_dict(contents)
+print(krewes)
