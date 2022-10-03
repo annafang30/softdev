dfbf0369be89e1f1ba94ae31780333b2676165dd
Anna Fang <afang30@stuy.edu>
2022-10-02 22:24:19 -0400

    added main comment block and starter code

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,1 +1,17 @@
+'''
+DISCO:
 
+QCC:
+
+HOW THIS SCRIPT WORKS:
+-  read in the csv file (using newLine, etc) and splice to return occupations and stats
+    * keys = occupations
+    * vals = percentages 
+'''
+# write a python script that reads the file and builds an appropriate dictionary from it. The percentages should be stored as numbers. Create a function that returns a randomly selected occuptation according to the given percentages. 
+
+# import the csv file and read 
+import csv
+file = open('occupations.csv')
+contents = file.read()
+print(contents) 
