ff2a544f0ff13164a0ea0e39094c981dde46a969
Anna Fang <afang30@stuy.edu>
2022-10-02 23:36:04 -0400

    import finished code using random.choices (inspo from piazza)

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -1,4 +1,10 @@
 '''
+Team AA: Anna Fang, Aden Garbutt
+SoftDev
+06_py-csv
+2022-10-03
+time spent:
+
 DISCO:
 
 QCC:
@@ -8,10 +14,32 @@ HOW THIS SCRIPT WORKS:
     * keys = occupations
     * vals = percentages 
 '''
-# write a python script that reads the file and builds an appropriate dictionary from it. The percentages should be stored as numbers. Create a function that returns a randomly selected occuptation according to the given percentages. 
+import random
+randomOcList = []
+randomPercents = []
 
-# import the csv file and read 
 import csv
 file = open('occupations.csv')
-contents = file.read()
-print(contents) 
+contents = file.readlines()
+
+# create the dictionary 
+occupationDict = {}
+
+#get the total percentage
+toReach = ((contents[len(contents)-1]).rsplit(',',1))[1]
+
+#read in all the information 
+def fillDict(inputs):
+    line = 1 # ignore the first and last lines
+    while line <= len(inputs)-2:
+        currentLine = inputs[line]
+        split = currentLine.rsplit(',',1)
+        occupationDict[split[0]] = []
+        randomOcList.append(split[0]) 
+        occupationDict[split[0]].append(float(split[1]))
+        randomPercents.append(float(split[1])) 
+        line+=1
+    # use random.choices to select based on weights 
+    print(random.choices(randomOcList, randomPercents, k = 1))
+    
+fillDict(contents)
