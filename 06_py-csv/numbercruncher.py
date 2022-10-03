7560a255d1dd8a8e6d92cb90bea6fa78dfb4730f
Anna Fang <afang30@stuy.edu>
2022-10-02 23:42:18 -0400

    add all DISCOs/QCC/and how script works

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -3,16 +3,22 @@ Team AA: Anna Fang, Aden Garbutt
 SoftDev
 06_py-csv
 2022-10-03
-time spent:
+time spent: 1.5 hours
 
 DISCO:
+- rsplice splices from the right - and you can choose how many times you want to splice the string
+- random.choice allows for weighing of results
 
 QCC:
+- is there a better way to splicing (while accounting for commas that may be within () or "" without using rsplice?
 
 HOW THIS SCRIPT WORKS:
--  read in the csv file (using newLine, etc) and splice to return occupations and stats
+-  read in the csv file (using newLine) and splice using rsplice to return occupations and stats in a list 
     * keys = occupations
-    * vals = percentages 
+    * vals = percentages
+- spliced strings are added to dictionary (occupationDict)
+    * percentages are typecasted to floats
+- lists for all keys and percentages are made and used to generate a random weighted result by using random.choices
 '''
 import random
 randomOcList = []
@@ -25,9 +31,6 @@ contents = file.readlines()
 # create the dictionary 
 occupationDict = {}
 
-#get the total percentage
-toReach = ((contents[len(contents)-1]).rsplit(',',1))[1]
-
 #read in all the information 
 def fillDict(inputs):
     line = 1 # ignore the first and last lines
@@ -35,7 +38,7 @@ def fillDict(inputs):
         currentLine = inputs[line]
         split = currentLine.rsplit(',',1)
         occupationDict[split[0]] = []
-        randomOcList.append(split[0]) 
+        randomOcList.append(split[0])
         occupationDict[split[0]].append(float(split[1]))
         randomPercents.append(float(split[1])) 
         line+=1
