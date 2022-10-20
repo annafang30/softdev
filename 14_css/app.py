bf95a177473234f6739c4128966200be81e47ddb
Anna Fang <afang30@stuy.edu>
2022-10-19 20:40:28 -0400

    add all headers

--- 14_css/app.py
+++ 14_css/app.py
@@ -1,6 +1,8 @@
-"""
-
-"""
+#Gastric Bypass Train - Sam Cowan, Anna Fang, Sadi Nirloy
+#K14: Serving Looks
+#SoftDev
+#19-10-2022
+#time spent: 0.7 hr
 
 from flask import Flask
 from flask import render_template
