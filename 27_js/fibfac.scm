a73b445d2e418956d7fb87b8bf92765d1a5b42e7
Anna Fang <afang30@stuy.edu>
2023-04-04 17:52:46 -0400

    pull from partner

--- 27_js/fibfac.scm
+++ 27_js/fibfac.scm
@@ -1,7 +1,7 @@
-;Team PAA :: Anna Fang, Aleksandra Shifrina
+;Team Double A V2 :: Anna Fang, Aleksandra Shifrina
 ;SoftDev pd7
 ;K27 -- Basic functions in JavaScript
-;2023-04-04f
+;2023-04-03t
 
 ;Scheme Antecedents for JS work
 
@@ -9,20 +9,29 @@
 ;<your team's fact(n) implementation>
 
 (define (fact n)
-    
-;(fact 1) ;"...should be  1"
-;(fact 2) ;"...should be  2"
-;(fact 3) ;"...should be  6"
-;(fact 4) ;"...should be  24"
-;(fact 5) ;"...should be  120"
+ (if (< n 2)
+  1)
+   (*n(fact (- n 1))))
+
+
+(fact 1) ;"...should be  1"
+;fact(2) ;"...should be  2"
+;fact(3) ;"...should be  6"
+;fact(4) ;"...should be  24"
+;fact(5) ;"...should be  120"
 
 
 ;fib:
 ;<your team's fib(n) implementation>
 
-;(fib 0) ;"...should be  0"
-;(fib 1) ;"...should be  1"
-;(fib 2) ;"...should be  1"
-;(fib 3) ;"...should be  2"
-;(fib 4) ;"...should be  3"
-;=================================================================
+(define (fib n)
+  (if (< n 2)
+    n
+    (+ (fib (- n 1)) (fib (- n 2)))))
+    
+;fib(0) ;"...should be  0"
+;fib(1) ;"...should be  1"
+;fib(2) ;"...should be  1"
+;fib(3) ;"...should be  2"
+;fib(4) ;"...should be  3"
+=================================================================
