6a00015f031611950533d3a079a7cad2769f1ce4
Anna Fang <afang30@stuy.edu>
2023-04-16 21:32:14 -0400

    pulled from sasha

--- 28_jsdom/dommy_consolery.js
+++ 28_jsdom/dommy_consolery.js
@@ -1,28 +1,41 @@
 /*
    your PPTASK:
-   
+
    Test drive each bit of code in this file,
     and insert comments galore, indicating anything
      you discover,
     	have questions about,
     		or otherwise deem notable.
-    		
+
     		Write with your future self or teammates in mind.
-    		
-    		If you find yourself falling out of flow mode, consult 
+
+    		If you find yourself falling out of flow mode, consult
     		other teams
     		MDN
 
    A few comments have been pre-filled for you...
-   
+
    (delete this block comment once you are done)
 */
 
-// Team Phantom Tollbooth :: Clyde Sinclair, Fierce Dragon 
-// SoftDev pd0
+// Team Double A V2 :: Sasha Shifrina, Anna Fang
+// SoftDev pd7
 // K28 -- Getting more comfortable with the dev console and the DOM
-// 2023-04-05w
+// 2023-04-16
 // --------------------------------------------------
+
+// DISCO
+// -----------------------------------------------------------------
+// *  console.log() sends a print call in the web console
+// *  pressing enter in the web console displays all log function at the bottom of the screen
+// *  console.log(myFxn) returns the body of the const
+// *  Ctrt+Shift/Option+J will allow you to run JS in the browser!
+// =================================================================
+//
+// QCC
+// -----------------------------------------------------------------
+// *  How can we employ the const myFxn? Can other functions envoke it?
+// =================================================================
 
 
 //send diagnostic output to console
@@ -86,9 +99,41 @@ var stripe = function() {
 
 //insert your implementations here for...
 // FIB
-// FAC
-// GCD
+function fib(n){
+    if(n<=1){
+        return n;
+    }
+    else{
+        return fib(n-1) + fib(n-2);
+    }
+}
 
+console.log(fib(9));
+console.log(fib(12));
+// FAC
+function fact(n){
+    if(n <= 1){
+        return 1;
+    }
+    else{
+        return n * fact(n-1);
+    }
+}
+
+console.log(fact(9));
+console.log(fact(12));
+// GCD
+function gcd(x, y){
+  while(y) {
+    var temp = y;
+    y = x % y;
+    x = temp;
+  }
+  return x;
+}
+
+console.log(gcd(3, 9));
+console.log(gcd(14,6));
 // In addition to the style shown above,
 //  you are encouraged to test drive the "arrow function syntax" as shown below.
 //  Note anything notable.
@@ -96,4 +141,4 @@ const myFxn = (param1, param2) => {
   // body
   return retVal;
 };
-
+console.log(myFxn);
