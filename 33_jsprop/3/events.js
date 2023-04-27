5c7d41f094283cee290978ab64e7a43d1b25bcea
Anna Fang <afang30@stuy.edu>
2023-04-27 13:11:24 -0400

    add classwork with weird issue

--- 33_jsprop/3/events.js
+++ 33_jsprop/3/events.js
@@ -8,7 +8,9 @@ var table = document.getElementsByTagName('table')[0];
 var clicky = function(e) {
   alert( this.innerHTML );
   //Q: What will happen when next line is uncommented?
-  //e.stopPropagation();
+  // predition: instead of the user having to manually close the popup, it'll automatically close 
+  // actual: only runs the first alert call and stops the rest of the loops
+  e.stopPropagation();
 };
 
 for (var x=0; x < tds.length; x++) {
@@ -21,9 +23,12 @@ for (x=0; x < trs.length; x++) {
 
 //Predict, then test...
 //Q: What effect does the boolean arg have?
+// prediction: displays when true and does not display when false 
+// actual: reverses order of display. When the boolean is entirely removed, the "boolean" is automatically set to false 
 //   (Leave exactly 1 version uncommented to test...)
 
-table.addEventListener('click', clicky, true);
+table.addEventListener('click', clicky,true);
 //table.addEventListener('click', clicky, false);
 
 // Q: When user clicks on a cell, in what order will the pop-ups appear?
+// same answer as the above question ^^ 
